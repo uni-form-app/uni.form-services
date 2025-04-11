828c72cff7b350ad32468830721d41934cd2f2c0
Jonas Neto <jonaseluna@gmail.com>
2025-04-10 22:55:44 -0300

    feat: validação dos parametros de parceiro

--- server/src/modules/partners/dto/create-partner.dto.ts
+++ server/src/modules/partners/dto/create-partner.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreatePartnerDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
+
+  @IsString()
+  @IsNotEmpty()
   addressId: string; // Tornado obrigatório
 }
