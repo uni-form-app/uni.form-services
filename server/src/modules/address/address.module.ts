93503671d3dd936ccc8d681098a45be55ee0fee0
Jonas Neto <jonaseluna@gmail.com>
2025-04-13 20:06:21 -0300

    bug: importando user no modulo de endereco

--- server/src/modules/address/address.module.ts
+++ server/src/modules/address/address.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AddressController],
   providers: [AddressService],
 })
