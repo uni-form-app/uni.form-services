095599688bedaee2486b6211360603f00f1f0af2
Jonas Neto <jonaseluna@gmail.com>
2025-04-19 17:11:57 -0300

    chore: env rabbit

--- api/src/config/env/index.ts
+++ api/src/config/env/index.ts
@@ -7,3 +7,10 @@ export const config = schema.env.parse({
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 })
+
+export const rabbitMQ = schema.rabbitMQ.parse({
+  URI: process.env.RABBIT_URI,
+  TOPICS: {
+    ORDER_CREATED: process.env.ORDER_CREATED,
+  }
+})
--- api/src/config/env/schema.ts
+++ api/src/config/env/schema.ts
@@ -1,8 +1,23 @@
 import { z } from 'zod';
 
-export const env = z.object({
+const rabbitTopcs = z.object({
+  ORDER_CREATED: z.string().default('order.created'),
+})
+
+const rabbitMQ = z.object({
+  URI: z.string().url(),
+  TOPICS: rabbitTopcs,
+})
+
+const env = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3000),
   JWT_SECRET: z.string().min(1),
   JWT_EXPIRES_IN: z.string().default('1h'),
 })
+
+export {
+  rabbitTopcs,
+  rabbitMQ,
+  env,
+}
