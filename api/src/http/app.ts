7cb98609239436498c11dc5cd3c8aa99a11b54d9
Jonas Neto <jonaseluna@gmail.com>
2025-04-21 19:05:47 -0300

    feat: expondo imagens public

--- api/src/http/app.ts
+++ api/src/http/app.ts
@@ -2,7 +2,12 @@ import express from "express";
 import { routers } from "../routes";
 import { app } from "./http";
 import { errorHandler } from "../middleware/errorHandler";
+import path from "path";
+
+const uploadsPath = path.resolve(__dirname, "../../", "uploads");
+
 app
   .use(express.json())
   .use("/api/v1/", routers)
+  .use("/public", express.static(uploadsPath))
   .use(errorHandler)
