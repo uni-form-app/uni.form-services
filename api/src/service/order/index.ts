960268f8319b30c0e73f33787349d1d789efddbc
Jonas Neto <jonaseluna@gmail.com>
2025-05-08 20:02:40 -0300

    feat:  atualizando produto

--- api/src/service/order/index.ts
+++ api/src/service/order/index.ts
@@ -1,9 +1,6 @@
 import { OrderStatus } from "@prisma/client";
-import { config } from "../../config/env";
 import pg from "../../libs/prisma";
-import { rabbit } from "../../libs/rabbitmq";
-import { productService } from "../product";
-import { Create, Get, GetUnique, Pay } from "./types";
+import { Create, Get, GetUnique } from "./types";
 
 export const get = async (args: Get.Args) => {
   const { userId, status } = args;
@@ -65,30 +62,24 @@ export const getUnique = async (args: GetUnique.Args) => {
 export const create = async (args: Create.Args) => {
   const { ...data } = args;
 
-  const product = await productService.getById(data.productId);
 
-  const order = await pg.order.create({
-    data
+  return pg.$transaction(async (client) => {
+    const order = await client.order.create({
+      data
+    });
+
+    await client.product.update({
+      where: {
+        id: order.id
+      },
+      data: {
+        status: "SOLD"
+      }
+    });
+
+    return order;
   });
-
-  return order;
 };
 
-export const pay = async (args: Pay.Args) => {
-  const { orderId } = args;
-
-  const order = await getUnique({ orderId });
-  if (!order) {
-    throw new Error("Order not found");
-  }
-
-  if (order.status !== "PENDING") {
-    throw new Error("Order is not pending");
-  }
-
-  await rabbit.publish<Pay.Args>(config.rabbitMQ.TOPICS.RABBIT_PAYMENT_PROCESS, {
-    orderId
-  })
-};
 
 export * as orderService from ".";
